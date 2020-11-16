329d8663c10fad4f595de5081ef1b9896ac202ae
John Dailey <johnjdailey@gmail.com>
2020-11-15 23:47:43 -0500

    Complete Day 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,41 @@
 
+#ancestor.py
+
+
+
+from collections import deque
+
+
+def createGraph(ancestors):
+    graph = {}
+    for edge in ancestors:
+        child, parent = edge[1], edge[0]
+    
+        if child in graph:
+            graph[child].add(parent)
+        else:
+            graph[child] = {parent}
+    return graph
+    
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    if len(ancestors) == 0:
+        return ''
+    graph = createGraph(ancestors) 
+    queue = deque()
+    queue.append(starting_node)
+    visited = set()
+    
+    if starting_node not in graph:
+        return -1
+    while len(queue) > 0:
+        curr = queue.popleft()
+        if curr not in visited:
+            visited.add(curr)
+            print(curr)
+
+            for neighbor in graph[curr]:
+                if neighbor in graph:
+                    queue.append(neighbor)
+    
+    return neighbor
